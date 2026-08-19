b8b022ffb2be6c1323031b5945a0ae57932e3c9e
Prabhu-Jethi <prabhujethi9@gmail.com>
2026-08-19 11:58:44 +0530

    subqueries commited

--- HackerRank_SQL/Sub-Queries/1_subquery.sql
+++ HackerRank_SQL/Sub-Queries/1_subquery.sql
@@ -1,84 +1,3 @@
--- DROP TABLE IF EXISTS students;
-
--- CREATE TABLE students(
---     stud_id SERIAL PRIMARY KEY,
---     stud_name VARCHAR(50) NOT NULL,
---     branch VARCHAR(20) NOT NULL
--- );
--- CREATE TABLE exam_score(
---     score_id SERIAL PRIMARY KEY,
---     stud_id INT NOT NULL REFERENCES students(stud_id),
---     subject VARCHAR(40) NOT NULL,
---     score INT NOT NULL CHECK (score BETWEEN 0 AND 100),
---     exam_month VARCHAR(10) NOT NULL
--- );
--- CREATE TABLE projects(
---     project_id SERIAL PRIMARY KEY,
---     stud_id INT NOT NULL REFERENCES students(stud_id),
---     title VARCHAR(50) NOT NULL,
---     marks INT NOT NULL CHECK (marks BETWEEN 0 AND 100)
--- );
-
--- INSERT into students (stud_name, branch) VALUES
--- ('Rohan', 'ME'),
--- ('Ashok', 'CS'),
--- ('Riaa', 'CE'),
--- ('Raghav', 'IT'),
--- ('Varun', 'CS'),
--- ('Kavya', 'IT'),
--- ('Meghaa', 'CS');
-
--- INSERT INTO exam_score (stud_id, subject, score, exam_month) VALUES
--- (1, 'Cloud', 62, '2026-02'),
--- (1, 'Operating', 78, '2026-03'),
--- (1, 'DSA', 58, '2026-03'),
--- (1, 'DBMS', 82, '2026-04'),
-
--- (2, 'Cloud', 52, '2026-02'),
--- (2, 'OS', 82, '2026-02'),
--- (2, 'DSA', 82, '2026-03'),
--- (2, 'DBMS', 56, '2026-03'),
-
--- (3, 'Cloud', 61, '2026-03'),
--- (3, 'OS', 77, '2026-03'),
--- (3, 'DSA', 98, '2026-03'),
--- (3, 'DBMS', 71, '2026-04'),
-
--- (4, 'Cloud', 51, '2026-02'),
--- (4, 'OS', 63, '2026-03'),
--- (4, 'DSA', 45, '2026-03'),
--- (4, 'DBMS', 72, '2026-04'),
-
--- (5, 'Cloud', 89, '2026-03'),
--- (5, 'OS', 87, '2026-03'),
--- (5, 'DSA', 65, '2026-03'),
--- (5, 'DBMS', 60, '2026-04'),
-
--- (6, 'Cloud', 75, '2026-02'),
--- (6, 'OS', 91, '2026-03'),
--- (6, 'DSA', 82, '2026-04'),
--- (6, 'DBMS', 60, '2026-04'),
-
--- (7, 'Cloud', 78, '2026-02'),
--- (7, 'OS', 43, '2026-02'),
--- (7, 'DSA', 55, '2026-03'),
--- (7, 'DBMS', 89, '2026-03');
-
-
--- INSERT INTO projects (stud_id, title, marks) VALUES
--- (1, 'Todo-app in react', 67),
--- (2, 'E-Commerce Platform', 81),
--- (3, 'Expense Tracker API', 63),
--- (4, 'Sentiment Analysis', 90),
--- (5, 'Portfolio website', 74),
--- (6, 'Chat bot with websockets', 78),
--- (7, 'RAG based Transformer', 89);
-
--- SELECT * from projects;
-
-
-
-
 
 --- SUB-QUERIES ---
 
@@ -160,3 +79,18 @@ INNER JOIN (
 --- which are above average (score > average class score). 
 -- Also include name of the student in the report.
 
+insert into high_score_report (
+    stud_id, 
+    stud_name, 
+    subject, 
+    score
+)
+SELECT s.stud_id, s.stud_name, e.subject, e.score
+from exam_score as e
+INNER JOIN students as s on s.stud_id = e.stud_id
+where e.score > (
+    SELECT avg(score)
+    from exam_score
+)
+
+SELECT * from high_score_report
